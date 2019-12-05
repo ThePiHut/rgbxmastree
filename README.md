83219e1418a301a402be435bcba2c1615865d3e5
Ben Nuttall <ben@bennuttall.com>
2019-12-05 22:30:03 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,133 @@
 # rgbxmastree
-Code example for the RGB Xmas Tree
+
+Code examples for the RGB Xmas Tree
+
+## Getting started
+
+Start by downloading the xmas tree file. Open a terminal and type:
+
+```bash
+wget http://goo.gl/rgbxmastree -O tree.py
+```
+
+When you write your own Python code, make sure you keep this file in the same
+folder.
+
+Open a Python shell or IDE (like Mu, Thonny or IDLE) and import `RGBXmasTree`:
+
+```python
+from tree import RGBXmasTree
+```
+
+Initialise your tree:
+
+```python
+from tree import RGBXmasTree
+
+tree = RGBXmasTree()
+```
+
+## Change the brightness
+
+You can change the brightness from 0 to 1 - the default is 0.5:
+
+```python
+from tree import RGBXmasTree
+
+tree = RGBXmasTree(brightness=0.1)
+```
+
+You'll find that 1 is _extremely bright_ and even 0.1 is plenty bright enough if
+the tree is on your desk :)
+
+## Change the colour
+
+You can set the colour of all the LEDs together using RGB values (all 0-1):
+
+```python
+from tree import RGBXmasTree
+
+tree = RGBXmasTree()
+
+tree.color = (1, 0, 0)
+```
+
+Alternatively you can use the `colorzero` library:
+
+```python
+from tree import RGBXmasTree
+from colorzero import Color
+
+tree = RGBXmasTree()
+
+tree.color = Color('red')
+```
+
+You can write a loop to repeatedly cycle through red, green and blue:
+
+```python
+from tree import RGBXmasTree
+from time import sleep
+
+tree = RGBXmasTree()
+
+colors = [(1, 0, 0), (0, 1, 0), (0, 0, 1)]
+
+for color in colors:
+    tree.color = color
+    sleep(1)
+```
+
+## Individual control
+
+You can also control each LED individually, for example turn each one red, one
+at a time:
+
+```python
+from tree import RGBXmasTree
+from time import sleep
+
+tree = RGBXmasTree()
+
+for pixel in tree:
+    pixel.color = (1, 0, 0)
+    sleep(1)
+```
+
+To control a specific pixel, you can access it by its index number (0-24):
+
+```python
+tree[0].color = (0, 1, 0)
+```
+
+## Random sparkles
+
+To randomly colour all the LEDs repeatedly, you need to set the value of the
+whole tree to a list of 25 random colour values. Start by writing a function to
+generate a random colour:
+
+```python
+from random import random
+
+def random_color():
+    r = random()
+    g = random()
+    b = random()
+    return (r, g, b)
+```
+
+Then create a second function to create a list of 25 random colours:
+
+```python
+from random import random
+
+def random_colors(n):
+    return [random_color() for i in range(n)]
+```
+
+Now write a loop to set the tree's value to this list:
+
+```python
+while True:
+    tree.value = random_colors(25)
+```
---
+++ tree.py
@@ -0,0 +1,107 @@
+from gpiozero import SPIDevice, SourceMixin
+from colorzero import Color, Hue
+from statistics import mean
+
+
+class Pixel(SourceMixin):
+    def __init__(self, parent, index):
+        super(Pixel, self).__init__()
+        self.parent = parent
+        self.index = index
+
+    @property
+    def value(self):
+        return self.parent.value[self.index]
+
+    @value.setter
+    def value(self, value):
+        new_parent_value = list(self.parent.value)
+        new_parent_value[self.index] = value
+        self.parent.value = tuple(new_parent_value)
+
+    @property
+    def color(self):
+        return Color(*self.value)
+
+    @color.setter
+    def color(self, c):
+        r, b, g = c
+        self.value = (r, g, b)
+
+    def on(self):
+        self.value = (1, 1, 1)
+
+    def off(self):
+        self.value = (0, 0, 0)
+
+
+class Tree(SourceMixin, SPIDevice):
+    def __init__(self, pixels, brightness=0.5, *args, **kwargs):
+        super(Tree, self).__init__(*args, **kwargs)
+        self._all = [Pixel(parent=self, index=i) for i in range(pixels)]
+        self._value = [(0, 0, 0)] * pixels
+        self.brightness = brightness
+        self.off()
+
+    def __len__(self):
+        return len(self._all)
+
+    def __getitem__(self, index):
+        return self._all[index]
+
+    def __iter__(self):
+        return iter(self._all)
+
+    @property
+    def color(self):
+        average_r = mean(pixel.color[0] for pixel in self)
+        average_g = mean(pixel.color[1] for pixel in self)
+        average_b = mean(pixel.color[2] for pixel in self)
+        return Color(average_r, average_g, average_b)
+
+    @color.setter
+    def color(self, c):
+        r, g, b = c
+        self.value = ((r, g, b),) * len(self)
+
+    @property
+    def brightness(self):
+        return self._brightness
+
+    @brightness.setter
+    def brightness(self, brightness):
+        max_brightness = 31
+        self._brightness_bits = int(brightness * max_brightness)
+        self._brightness = brightness
+        self.value = self.value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        start_of_frame = [0]*4
+        end_of_frame = [0]*5
+                     # SSSBBBBB (start, brightness)
+        brightness = 0b11100000 | self._brightness_bits
+        pixels = [[int(255*v) for v in p] for p in value]
+        pixels = [[brightness, b, g, r] for r, g, b in pixels]
+        pixels = [i for p in pixels for i in p]
+        data = start_of_frame + pixels + end_of_frame
+        self._spi.transfer(data)
+        self._value = value
+
+    def on(self):
+        self.value = ((1, 1, 1),) * len(self)
+
+    def off(self):
+        self.value = ((0, 0, 0),) * len(self)
+
+    def close(self):
+        super(Tree, self).close()
+
+if __name__ == '__main__':
+    from time import sleep
+
+    tree = Tree(25, mosi_pin=12, clock_pin=25)
