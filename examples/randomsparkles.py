028d5a8d61c3502af0acc465af497b7af6ef3927
Ben Nuttall <ben@bennuttall.com>
2019-12-17 21:31:01 +0000

    Simplify random sparkles example

--- examples/randomsparkles.py
+++ examples/randomsparkles.py
@@ -1,19 +1,17 @@
 from tree import RGBXmasTree
-from random import random
+import random
 
 tree = RGBXmasTree()
 
 def random_color():
-    r = random()
-    g = random()
-    b = random()
+    r = random.random()
+    g = random.random()
+    b = random.random()
     return (r, g, b)
-
-def random_colors(n):
-    return [random_color() for i in range(n)]
 
 try:
     while True:
-        tree.value = random_colors(25)
+        pixel = random.choice(tree)
+        pixel.color = random_color()
 except KeyboardInterrupt:
     tree.close()
